2e1ef33f3e2c4e2fb036bfb7cdbeb6c1bf34c42a
jonatan <jonatan.e.vera@gmail.com>
2016-07-14 19:03:26 -0300

    Se agregan las vistas: - insertar.jsp - eliminar.jsp - modificar.jsp - listar.jsp
    Se agrega al archivo pom.xml la dependencia del mysql-connector

---
+++ src/main/java/com/enlawebdekaaf/appwebcrud/Conexion.java
@@ -0,0 +1,60 @@
+package com.enlawebdekaaf.appwebcrud;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.exceptions.MySQLDataException;
+
+public class Conexion {
+	private final String servidor="jdbc:mysql://localhost/dbappwebconexionjdbc";
+	private final String usuario="root";
+	private final String contrasenia="123456";
+	private Connection conexion=null;
+
+	public Conexion()
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			conexion=(Connection)DriverManager.getConnection(servidor, usuario, contrasenia);
+		}
+		catch(ClassNotFoundException ex)
+		{
+			System.out.println("No se encontró la clase para la conexión de base de datos");
+		}
+		catch(MySQLDataException ex)
+		{
+			System.out.println("Error en MySqlDataException");
+		}
+		catch(SQLException ex)
+		{
+			System.out.println("Error en Driver Manager");
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Ocurrió un error inesperado en la conexión");
+		}
+	}
+	
+	public void cerrarConexion()
+	{
+		if(conexion!=null)
+		{
+			try
+			{
+				conexion.close();
+				conexion=null;
+			}
+			catch(Exception ex)
+			{
+				System.out.println("No se pudo cerrar la conexión");
+			}
+		}
+	}
+	
+	public Connection getConexion() {
+		return conexion;
+	}
+
+}
